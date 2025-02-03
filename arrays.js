aec0a9f55f3fd644c1db974176a5bf9979c8f54f
Aayush Pradhan <muma5581@gmail.com>
2025-02-03 13:59:00 +0545

    third commit

--- arrays.js
+++ arrays.js
@@ -1,1 +1,27 @@
-// 
+// arrays in js
+// const person1= "ram";
+// const person2= "shyam";
+// const person3= "hari";
+
+// const persons = ["shyam"," ram", "hari"];
+const numbers = [1,2,3,4] // more usage
+
+// const arr = new Array() //use less 
+
+console.log(numbers.length);
+
+// console.log(arr.length);
+
+numbers[0] = 1;
+
+// console.log(numbers.length);
+// console.log(numbers);
+
+console.log(numbers.indexOf(1));
+
+numbers[2] = 5 // can change the data inside the array;
+
+// numbers.push(10) //add to the end 
+numbers.pop() // delete from end and return 
+
+console.log(numbers)
